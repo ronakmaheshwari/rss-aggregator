f61d6f5c2b151443db72496390d62de00018f681
ronak maheshwari <ronakmaheshwari077@gmail.com>
2026-04-21 23:36:31 +0530

    Server Init

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/ronakmaheshwari/rss-aggregator
 
-go 1.26.2
+go 1.22
+
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.2
--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello aggregator")
+	godotenv.Load()
+
+	port :=  os.Getenv("PORT");
+	if port == "" {
+		log.Fatal(`Port must be set`)
+	}
+	
+	router := chi.NewRouter();
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+  	}))
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":" + port,
+	}
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	fmt.Printf(`RSS Aggregator is running on http://localhost:%v`, port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
